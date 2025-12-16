2d07743c12716eb96ed513c3a298f913c09b2639
limnimoldev-netizen <limnimol.dev@gmail.com>
2025-12-16 16:51:31 +0700

    start navegation

--- wp-content/themes/my-ecommerce-theme/front-page.php
+++ wp-content/themes/my-ecommerce-theme/front-page.php
@@ -22,48 +22,486 @@
     <h1>Categories</h1>
     <div class="products">
         <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/straw.png" alt="<?php bloginfo('jinRo Mart'); ?>">
             <p class="name">Jinro Soju</p>
         </div>
         <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/ora.png" alt="<?php bloginfo('jinRo Mart'); ?>">
             <p class="name">Jinro Soju</p>
         </div>
         <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/pea.png" alt="<?php bloginfo('jinRo Mart'); ?>">
             <p class="name">Jinro Soju</p>
         </div>
         <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/gra.png" alt="<?php bloginfo('jinRo Mart'); ?>">
             <p class="name">Jinro Soju</p>
         </div>
-        
         
     </div>
 </section>
 
 <section class="Feature">
     <h1>Feature Products</h1>
-    <div class="products">
-    <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
-            <p class="name">Jinro Soju</p>
-    </div>
-    <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
-            <p class="name">Jinro Soju</p>
+    <div class="container-products">
+        
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/soda2.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+     <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/juice2.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/juice1.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/juice.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
     </div>
 
-    <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
-            <p class="name">Jinro Soju</p>
+    <div class="container-products">
+        
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/tea2.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+     <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/jinro2.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/jinro1.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/tea1.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+      
     </div>
-    <div class="items">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/image/product.png" alt="<?php bloginfo('jinRo Mart'); ?>">
-            <p class="name">Jinro Soju</p>
+
+    <section>
+        <div class="lates">
+            <h1>Latest Products</h1>
+        </div>
+         <div class="container-products">
+        
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/soda1.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+     <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/milk1.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/soda.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+
+
+
+        <div class="product-card">
+            <div class="items">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/jinro1.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="rating">
+                <div class="container-icon">
+                    <span>korea</span>
+                    <div class="icon-rate">
+                        <div class="star-icon">
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <span>4.7</span>
+                    </div>
+                </div>
+                <div class="usd-name">
+                    <h2 class="name-pro">Jinro Chamisul - Peach</h2>
+                    <span class="detail">360ml that was  to embody the unique <br> sweet taste.</span>
+                    <h1 class="usd">USD 10.65</h1>
+                </div>
+                    
+            </div>
+            <div class="btn-add">
+                <button class="card-btn">Add to card</button>
+            </div>
+        </div>
+      
     </div>
-    
 </section>
+
+<section class="container-delivery">
+    <h1>Special Benefit</h1>
+    
+    <div class="container-box">
+        <div class="delivery">
+            <div class="car-icon">
+                <i class="fas fa-truck"></i>
+            </div>
+            
+            <div class="detail-delivery">
+                <h2>Free Delivery</h2>
+                <p>We offer fast and reliable delivery so you can enjoy your favorite Korean drinks
+                 and snacks without waiting too long. Shopping at JinRo Mart becomes easy and 
+                 convenient with quick delivery to your door.</p>
+            </div>
+        </div>
+
+        <div class="delivery">
+            <div class="car-icon">
+               <i class="fas fa-fire-alt"></i>
+            </div>
+            
+            <div class="detail-delivery">
+                <h2>Hot Items</h2>
+                <p>We offer fast and reliable delivery so you can
+                     enjoy your favorite Korean drinks and snacks without waiting too long. Shopping at 
+                     JinRo Mart becomes easy and convenient with quick delivery to your door.</p>
+            </div>
+        </div>
+
+        <div class="delivery">
+            <div class="car-icon">
+                <i class="fas fa-star"></i> 
+            </div>
+            
+            <div class="detail-delivery">
+                <h2>Free Delivery</h2>
+                <p>We offer fast and reliable delivery so you can enjoy your favorite Korean drinks
+                 and snacks without waiting too long. Shopping at JinRo Mart becomes easy and 
+                 convenient with quick delivery to your door.</p>
+            </div>
+        </div>
+    </div>
+        
+</section>
+
+
+ <section>
+    <h1>Customer Stories</h1>
+    <div class="container-customer">
+        <div class="name-custom">
+            <div class="img-pf">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/pf1.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="name-pf">
+                <h3>Jeong-hwa</h3>
+                <span>Korean</span>
+                <p>Customer service was fantastic. They helped me choose the right product, guided me through the options,
+                     and made sure I was confident in my choice. 
+                     I couldn’t be happier with my purchase.</p>
+            </div>
+
+        </div>
+
+        <div class="name-custom">
+            <div class="img-pf">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/pf3.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="name-pf">
+                <h3>Jeong-hwa</h3>
+                <span>Korean</span>
+                <p>Customer service was fantastic. They helped me choose the right product, guided me through the options,
+                     and made sure I was confident in my choice. 
+                     I couldn’t be happier with my purchase.</p>
+            </div>
+
+        </div>
+
+        <div class="name-custom">
+            <div class="img-pf">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/image/pf2.png" alt="<?php bloginfo('jinRo Mart'); ?>">
+            </div>
+            
+            <div class="name-pf">
+                <h3>Jeong-hwa</h3>
+                <span>Korean</span>
+                <p>Customer service was fantastic. They helped me choose the right product, guided me through the options,
+                     and made sure I was confident in my choice. 
+                     I couldn’t be happier with my purchase.</p>
+            </div>
+        </div>
+    </div>   
+ </section>
+
+ <section>
+    <div class="nevgation-contain">
+        <div class="btn-nev">
+            
+        </div>
+    </div>
+ </section>
 
 
 
--- wp-content/themes/my-ecommerce-theme/header.php
+++ wp-content/themes/my-ecommerce-theme/header.php
@@ -2,68 +2,30 @@
 <html <?php language_attributes(); ?>>
 <head>
     <?php wp_head(); ?>
-   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 </head>
 <body <?php body_class(); ?>>
-<header>
-    <div class="logo-menu">
-        <div class="logo">
+    <header>
+        <!-- <h1><?php bloginfo('name'); ?></h1> -->
+         <div class="logo">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/image/logo.png" alt="<?php bloginfo('jinRo Mart'); ?>">
         </div>
-        <div class="search-wrapper">
-            <form role="search" method="get" action="<?php echo home_url('/'); ?>" class="search-form">
-                <button type="submit" class="search-btn">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-                <input
-                    type="search"
-                    class="search-input"
-                    placeholder="Search here..."
-                    value="<?php echo get_search_query(); ?>"
-                    name="s">
-            </form>
-        </div>
-
-        <div class="icon">
-            <a href="">
-                <i class="fas fa-user"></i>
-            <span>login/sigup</span>
-            </a>
-        </div>
-
-        <div class="icon1">
-            <a href="">
+        <?php wp_nav_menu(['theme_location'=>'primary']); ?>
+        <div class="icon-container">
+            <div class="signin">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <span>login/signup</span>
+                
+            </div>
+            <div class="icon">
                 <i class="fas fa-shopping-bag"></i>
-            </a>
-        </div>
+            </div>
+        </div> 
+    </header>
 
-        
+    
 
-    </div>
-
-
-        <div class="menu">
-            <ul>
-                <li>
-                    <a href="">Home</a>
-                </li>
-                <li>
-                    <a href="">About</a>
-                </li>
-                <li>
-                    <a href="">Categories</a>
-                </li>
-                <li>
-                    <a href="">Products</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
-            </ul>
-        </div>
-        
-     </div>
-
-</header>
 <main>
